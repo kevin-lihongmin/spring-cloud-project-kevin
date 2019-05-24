144798fa31ecbbcf18f2bc55da36056adb6a77f1
unknown <it_lihongmin@163.com>
2019-05-25 00:23:29 +0800

    配置客户端

--- spring-cloud-config/config-client/src/main/java/com/kevin/config/client/ConfigClientApplication.java
+++ spring-cloud-config/config-client/src/main/java/com/kevin/config/client/ConfigClientApplication.java
@@ -1,7 +1,12 @@
 package com.kevin.config.client;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.refresh.ContextRefresher;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.Set;
 
 @SpringBootApplication
 public class ConfigClientApplication {
@@ -11,4 +16,21 @@ public class ConfigClientApplication {
 		SpringApplication.run(ConfigClientApplication.class, args);
 	}
 
+	private ContextRefresher contextRefresher;
+
+	@Autowired
+	public ContextRefresher getContextRefresher() {
+		return contextRefresher;
+	}
+
+	/**
+	 *  定时刷新配置
+	 */
+	@Scheduled(fixedRate = 1000L)
+	public void refreshInterval() {
+		Set<String> refresh = contextRefresher.refresh();
+		if (!refresh.isEmpty()) {
+			System.out.println(refresh.toString());
+		}
+	}
 }
