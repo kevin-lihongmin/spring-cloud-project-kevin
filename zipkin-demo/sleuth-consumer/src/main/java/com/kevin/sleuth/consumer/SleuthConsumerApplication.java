9fefd5787a45560f8cc4829bfa6706b72e05413e
it_lihongmin@163.com <it_lihongmin@163.com>
2019-06-06 14:05:16 +0800

    使用注册中心发现方式调用服务

--- zipkin-demo/sleuth-consumer/src/main/java/com/kevin/sleuth/consumer/SleuthConsumerApplication.java
+++ zipkin-demo/sleuth-consumer/src/main/java/com/kevin/sleuth/consumer/SleuthConsumerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -30,6 +32,7 @@ import org.springframework.web.client.RestTemplate;
  */
 @EnableEurekaClient
 @SpringBootApplication
+@RibbonClients({@RibbonClient(name = "sleuth-provider")})
 public class SleuthConsumerApplication {
 
 	public static void main(String[] args) {
--- zipkin-demo/sleuth-provider/src/main/java/com/kevin/sleuth/provider/SleuthProviderApplication.java
+++ zipkin-demo/sleuth-provider/src/main/java/com/kevin/sleuth/provider/SleuthProviderApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -17,6 +19,7 @@ import org.springframework.web.client.RestTemplate;
  */
 @EnableEurekaClient
 @SpringBootApplication
+@RibbonClients({@RibbonClient(name = "sleuth-provider-provider")})
 public class SleuthProviderApplication {
 
 	public static void main(String[] args) {
